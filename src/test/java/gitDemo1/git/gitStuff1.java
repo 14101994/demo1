3272f643b096189c61d192f2b114987146c54778
akshay <akshay.zagade14@gmail.com>
2023-02-16 18:19:40 +0530

    commit from develop branch

--- src/test/java/gitDemo1/git/gitStuff1.java
+++ src/test/java/gitDemo1/git/gitStuff1.java
@@ -5,6 +5,12 @@ public class gitStuff1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("comment from gitstuff workspace");
+		
+		System.out.println("comment from gitstuff workspace111");
+		
+		System.out.println("comment from gitstuff workspace222");
+		
+		System.out.println("comment from gitstuff workspace333");
 
 	}
 
--- src/test/java/gitDemo1/git/sample1.java
+++ src/test/java/gitDemo1/git/sample1.java
@@ -8,6 +8,10 @@ public class sample1 {
 		System.out.println("hello");
 		
 		System.out.println("updated for fisrt time for git demo");
+		
+System.out.println("hello 123123 123123 " );
+		
+		System.out.println("updated for fisrt time for git demo 123 123 ");
 
 	}
 
