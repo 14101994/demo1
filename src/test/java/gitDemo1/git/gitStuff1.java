ac74c86b5f76608d830d953b97b783263c875a47
akshay <akshay.zagade14@gmail.com>
2023-02-16 17:22:09 +0530

    commit from git workspace

---
+++ src/test/java/gitDemo1/git/gitStuff1.java
@@ -0,0 +1,11 @@
+package gitDemo1.git;
+
+public class gitStuff1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("comment from gitstuff workspace");
+
+	}
+
+}
