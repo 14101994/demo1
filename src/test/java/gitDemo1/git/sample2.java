337649edb8068921a2baf2de05d26974e42bd13e
akshay <akshay.zagade14@gmail.com>
2023-02-16 19:43:12 +0530

    from new branch newsample

---
+++ src/test/java/gitDemo1/git/sample2.java
@@ -0,0 +1,18 @@
+package gitDemo1.git;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("gits stuff 111");
+		
+		System.out.println("gits stuff 222");
+		
+		System.out.println("gits stuff 333");
+		
+		System.out.println("gits stuff 444");
+
+	}
+
+}
